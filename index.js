d7ad503dc2e896a68c8df954dcfc0c041a68b5b4
Joao Pedro Santiago Ferreira <jplok1025@outlook.com>
2025-06-26 22:42:02 -0300

    .

--- index.js
+++ index.js
@@ -124,10 +124,10 @@ app.get('/batepapo.html', protegePagina, (req, res) => {
       <h1>Entrar no Bate-papo</h1>
       <form method="POST" action="/batepapo">
         <label>Digite seu nickname:<br>
-          <input type="text" name="nickname">
+          <input type="text" name="nickname" required>
         </label><br><br>
         <label>Digite o assunto:<br>
-          <input type="text" name="assunto">
+          <input type="text" name="assunto" required>
         </label><br><br>
         <button>Entrar</button>
       </form>
